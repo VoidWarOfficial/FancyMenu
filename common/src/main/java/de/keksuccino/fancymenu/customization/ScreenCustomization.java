080c4d21a7a72f017ab534904855c5ecd96908bd
VoidWarOfficial <144384146+VoidWarOfficial@users.noreply.github.com>
2024-06-03 04:36:06 -0400

    Update ScreenCustomization.java

--- common/src/main/java/de/keksuccino/fancymenu/customization/ScreenCustomization.java
+++ common/src/main/java/de/keksuccino/fancymenu/customization/ScreenCustomization.java
@@ -339,7 +339,6 @@ public class ScreenCustomization {
 		addScreenBlacklistRule((screen) -> screen.startsWith("eu.midnightdust."));
 		addScreenBlacklistRule((screen) -> screen.startsWith("net.cobrasrock.skinswapper."));
 		addScreenBlacklistRule((screen) -> screen.equals(VideoSettingsScreen.class.getName()) && Compat.isOptiFineLoaded());
-		addScreenBlacklistRule((screen) -> screen.startsWith("de.keksuccino.fancymenu.") && !screen.equals(CustomGuiBaseScreen.class.getName()));
 
 	}
 
